8e07bb751810d92aa395b9956f021e45e419c36d
Nick Klose <nick.klose@ualberta.ca>
2017-01-22 23:46:03 -0700

    moved user registration to User class; closes #12

--- src/User.py
+++ src/User.py
@@ -1,3 +1,4 @@
+import re
 import time
 
 import GameController as gc
@@ -135,6 +136,110 @@ class User:
                 self.name = username
                 self.lookup()
                 valid_creds = True
+
+        # close database
+        db.close()
+
+    def register(self):
+        gc.msg('\nStarting the account creation process...')
+        gc.msg('Your username can be between 4 and 16 characters and must be alphanumeric.')
+        valid_user = False
+        db = Database()
+        while not valid_user:
+            self.name = raw_input('Desired Username: ')
+
+            # name cannot be blank
+            if self.name == '':
+                gc.error('You must enter a username.')
+            # name must be alphanumeric; symbols are rejected prior to database lookup
+            elif not self.name.isalnum():
+                gc.error('Your username can only contain letters and numbers.')
+            # name must be between 4 and 16 characters
+            elif len(self.name) < 4 or len(self.name) > 16:
+                gc.error('Your username must be between 4 and 16 characters.')
+            # name must not already exist in database
+            else:
+                sql = 'SELECT * FROM users WHERE username = %s;'
+                response = db.get_query(sql, [self.name])
+                if len(response) > 0:
+                    gc.error('Sorry, that name is taken. Please choose another.')
+                else:
+                    gc.msg('Username is available!')
+                    valid_user = True
+
+        # get a valid password
+        gc.msg('\n  You can now set a password for your account.')
+        valid_password = False
+        while not valid_password:
+            password = getpass()
+            confirm = getpass('Confirm: ')
+            if len(password) < 6:
+                gc.error('Your password must contain 6 or more characters.')
+            elif password != confirm:
+                gc.error('Sorry, those passwords didn\'t match.')
+            else:
+                valid_password = True
+                self.password = gc.hash_password(password, self.name)
+
+        # get a valid email address
+        valid_email = False
+        gc.msg('\n  You will need to enter a valid email in case you forget your password.')
+        while not valid_email:
+            self.email = raw_input('Email Address: ')
+            if re.match(r'[^@]+@[^@]+\.[^@]+', self.email):
+                # check if email exists in database
+                sql = 'SELECT * FROM users WHERE email = %s;'
+                response = db.get_query(sql, [self.email])
+                if len(response) > 0:
+                    gc.error('Sorry, that email has already been registered.')
+                else:
+                    valid_email = True
+            else:
+                gc.error('Sorry, your input was not in the right format for an email address.')
+
+        # get a unique in-game handle
+        gc.msg('\n  Finally, you need to choose an in-game handle that others can see.')
+        gc.msg('This is separate from your username, and can be changed later.')
+        valid_handle = False
+        while not valid_handle:
+            self.handle = raw_input('Please enter a handle: ')
+            if self.handle == '':
+                gc.error('You must enter a handle.')
+            elif not self.handle.isalnum():
+                gc.error('Your handle can only contain letters and numbers.')
+            elif len(self.handle) < 2 or len(self.handle) > 16:
+                gc.error('Your handle must be between 2 and 16 characters.')
+            else:
+                sql = 'SELECT * FROM users WHERE handle = %s;'
+                response = db.get_query(sql, [self.handle])
+                if len(response) > 0:
+                    gc.error('Sorry, that handle is taken. Please choose another.')
+                else:
+                    valid_handle = True
+
+        # generate an IP address for the user
+        valid_ip = False
+        while not valid_ip:
+            self.computer.ip = gc.gen_ip()
+            # check if the IP has been assigned already
+            sql = 'SELECT * FROM computers WHERE ip = %s;'
+            response = db.get_query(sql, [self.computer.ip])
+            if len(response) == 0:
+                valid_ip = True
+
+        self.save()     # write user to database
+        self.lookup()   # get generated id for the user
+
+        # update computer information
+        self.computer.password = gc.gen_password()
+        self.computer.owner_id = self.id
+
+        self.computer.save()     # write computer to database
+        self.computer.lookup()   # get generated id for computer
+
+        self.save()
+
+        gc.success('\n Account ' + self.name + ' created!')
 
         # close database
         db.close()
--- src/game.py
+++ src/game.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-import re
 import signal
 import subprocess
 import pwd
@@ -86,108 +85,7 @@ def login():
 # Register a new user account
 def register():
     user = User()
-    gc.msg('\nStarting the account creation process...')
-    gc.msg('Your username can be between 4 and 16 characters and must be alphanumeric.')
-    valid_user = False
-    db = Database()
-    while not valid_user:
-        user.name = raw_input('Desired Username: ')
-
-        # name cannot be blank
-        if user.name == '':
-            gc.error('You must enter a username.')
-        # name must be alphanumeric; symbols are rejected prior to database lookup
-        elif not user.name.isalnum():
-            gc.error('Your username can only contain letters and numbers.')
-        # name must be between 4 and 16 characters
-        elif len(user.name) < 4 or len(user.name) > 16:
-            gc.error('Your username must be between 4 and 16 characters.')
-        # name must not already exist in database
-        else:
-            sql = 'SELECT * FROM users WHERE username = %s;'
-            response = db.get_query(sql, [user.name])
-            if len(response) > 0:
-                gc.error('Sorry, that name is taken. Please choose another.')
-            else:
-                gc.msg('Username is available!')
-                valid_user = True
-
-    # get a valid password
-    gc.msg('\n  You can now set a password for your account.')
-    valid_password = False
-    while not valid_password:
-        password = getpass()
-        confirm = getpass('Confirm: ')
-        if len(password) < 6:
-            gc.error('Your password must contain 6 or more characters.')
-        elif password != confirm:
-            gc.error('Sorry, those passwords didn\'t match.')
-        else:
-            valid_password = True
-            user.password = gc.hash_password(password, user.name)
-
-    # get a valid email address
-    valid_email = False
-    gc.msg('\n  You will need to enter a valid email in case you forget your password.')
-    while not valid_email:
-        user.email = raw_input('Email Address: ')
-        if re.match(r'[^@]+@[^@]+\.[^@]+', user.email):
-            # check if email exists in database
-            sql = 'SELECT * FROM users WHERE email = %s;'
-            response = db.get_query(sql, [user.email])
-            if len(response) > 0:
-                gc.error('Sorry, that email has already been registered.')
-            else:
-                valid_email = True
-        else:
-            gc.error('Sorry, your input was not in the right format for an email address.')
-
-    # get a unique in-game handle
-    gc.msg('\n  Finally, you need to choose an in-game handle that others can see.')
-    gc.msg('This is separate from your username, and can be changed later.')
-    valid_handle = False
-    while not valid_handle:
-        user.handle = raw_input('Please enter a handle: ')
-        if user.handle == '':
-            gc.error('You must enter a handle.')
-        elif not user.handle.isalnum():
-            gc.error('Your handle can only contain letters and numbers.')
-        elif len(user.handle) < 2 or len(user.handle) > 16:
-            gc.error('Your handle must be between 2 and 16 characters.')
-        else:
-            sql = 'SELECT * FROM users WHERE handle = %s;'
-            response = db.get_query(sql, [user.handle])
-            if len(response) > 0:
-                gc.error('Sorry, that handle is taken. Please choose another.')
-            else:
-                valid_handle = True
-
-    # generate an IP address for the user
-    valid_ip = False
-    while not valid_ip:
-        user.computer.ip = gc.gen_ip()
-        # check if the IP has been assigned already
-        sql = 'SELECT * FROM computers WHERE ip = %s;'
-        response = db.get_query(sql, [user.computer.ip])
-        if len(response) == 0:
-            valid_ip = True
-
-    user.save()     # write user to database
-    user.lookup()   # get generated id for the user
-
-    # update computer information
-    user.computer.password = gc.gen_password()
-    user.computer.owner_id = user.id
-
-    user.computer.save()     # write computer to database
-    user.computer.lookup()   # get generated id for computer
-
-    user.save()
-
-    gc.success('\n Account ' + user.name + ' created!')
-
-    # close database
-    db.close()
+    user.register()
     profile(user)
 
 # Establish a connection to IRC
